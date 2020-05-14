4780ef45429f741cd4206cfdc3a263db14089e59
n1como <n1como@163.com>
2020-05-14 08:57:27 +0800

    add exception package

---
+++ src/main/java/exception/UserDefinedException.java
@@ -0,0 +1,36 @@
+package exception;
+
+public class UserDefinedException {
+    public static void handleExceptions(UserDefinedException obj) {
+        try {
+            obj.method1();
+            obj.method2();
+            obj.method3();
+        } catch (Exception e) {
+            printStack(e);
+        }
+    }
+
+    public static void printStack(Throwable throwable) {
+        System.out.println(throwable);
+        for (StackTraceElement element : throwable.getStackTrace()) {
+            System.out.println(element);
+        }
+    }
+
+    public void method1() {
+        throw new NullPointerException();
+    }
+
+    public void method2() {
+        throw new IndexOutOfBoundsException();
+    }
+
+    public void method3() {
+        throw new NumberFormatException();
+    }
+
+    public static void main(String[] args) {
+        handleExceptions(new UserDefinedException());
+    }
+}
